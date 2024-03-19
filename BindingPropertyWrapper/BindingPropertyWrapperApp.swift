4941cada19589fe3a79b7e6d91738eb75683822d
Sadia Tasnim <tasnimsadia259@gmail.com>
2024-03-19 10:54:03 +0600

    Initial Commit

---
+++ BindingPropertyWrapper/BindingPropertyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingPropertyWrapperApp.swift
+//  BindingPropertyWrapper
+//
+//  Created by Sadia on 19/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BindingPropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
